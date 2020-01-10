df4d7cba440db0cb9b967ece1f72061f34ddcbde
Bojan Micic <bojanmicic89@gmail.com>
2020-01-10 19:39:27 +0100

    adds test for finding a gallery

--- cypress/integration/tests/findGallery.spec.js
+++ cypress/integration/tests/findGallery.spec.js
@@ -2,15 +2,19 @@ const locators = require('../../fixtures/locators.json')
 
 describe('These tests check if users can use search functionality', () => {
 
+    let galname = "Hey VivifyIdeas"
+
     before(() => {
+        cy.apiLogin("main")
         cy.visit("/")
     })
 
-    it('This test checks if users can search for a new QA Job', () => {
+    it('This test checks if users can search for a gallery', () => {
         cy
-            .wait(2000)
-            .get(locators.FIND.SEARCHINPUT).type("QA Engineer")
+            .get(locators.FIND.SEARCHINPUT).type(galname)
             .get(locators.FIND.FILTERBTN).click()
+            .get(".container")
+                .should('contain', galname)
     })
 
     after(() => {
